b3eacc91b335623ff4e2f8430a7bca4c68312458
Cristian Salamea <salamea@vauxoo.com>
2019-12-02 23:10:37 -0600

    [FIX] removed use-package

--- core/init-flycheck.el
+++ core/init-flycheck.el
@@ -1,6 +1,14 @@
+;;;  package --- Summary:
+
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'flycheck)
 
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 (provide 'init-flycheck)
 
+;;; init-flycheck.el ends here
